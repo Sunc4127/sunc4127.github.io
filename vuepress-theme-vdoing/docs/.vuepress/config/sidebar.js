820d6d39954591f81a5e363e6f8eed993e936d85
sunc4 <sunc4@student.unimelb.edu.au>
2021-11-11 11:11:28 +0800

    Revert "delete es6 related files"
    This reverts commit 3c8fc326bce1938b21809b5c52c00de50c2400c7.

--- vuepress-theme-vdoing/docs/.vuepress/config/sidebar.js
+++ vuepress-theme-vdoing/docs/.vuepress/config/sidebar.js
@@ -10,6 +10,7 @@ module.exports = {
         ['01.JavaScript/01.JavaScript中的名词概念','JavaScript中的名词概念'],
         ['01.JavaScript/02.数据类型转换','数据类型转换'],
         ['01.JavaScript/03.ES5面向对象','ES5面向对象'],
+        ['01.JavaScript/04.ES6面向对象','ES6面向对象'],
         ['01.JavaScript/05.new命令原理','new命令原理'],
         ['01.JavaScript/06.多种数组去重性能对比','多种数组去重性能对比'],
       ]
