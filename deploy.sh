8deecec45bb232f7814758b69c1033b722c22636
Sunc4127 <sccplay888@gmail.com>
2021-11-13 08:31:46 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
 
 # deploy to github pages
 #git push -f git@github.com:<Sunc4127>/<Sunc4127>.github.io.git master
@@ -24,20 +24,8 @@ fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl main:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl HEAD:main # 推送到github gh-pages分支
 
-# deploy to coding pages
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
