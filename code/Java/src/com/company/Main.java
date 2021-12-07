658ac0ad5bc531b7b0bedb7d95cad3b3735dc56e
Sunc4127 <sccplay888@gmail.com>
2021-12-07 15:31:12 +0800

    add java 02 codes

--- code/Java/src/com/company/Main.java
+++ code/Java/src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         // class, member (field, method), member (method)
-        System.out.println("Hello world");
+        System.out.println("Start");
+        printNumbers(4);
+        System.out.println("End");
+    }
+
+    public static void printNumbers(int limit) {
+        for (int i = 0; i < limit; i += 2) {
+            System.out.println(i);
+        }
     }
 }
