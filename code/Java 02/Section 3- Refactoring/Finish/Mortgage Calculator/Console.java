dfa6fa781b2494c3c910543c05d5e48265a1a6d6
Sunc4127 <sccplay888@gmail.com>
2021-12-15 17:25:16 +0800

    add notes

--- code/Java 02/Section 3- Refactoring/Finish/Mortgage Calculator/Console.java
+++ code/Java 02/Section 3- Refactoring/Finish/Mortgage Calculator/Console.java
@@ -2,22 +2,26 @@ package com.codewithmosh;
 
 import java.util.Scanner;
 
+
 public class Console {
-  private static Scanner scanner = new Scanner(System.in);
 
-  public static double readNumber(String prompt) {
-      return scanner.nextDouble();
-  }
+    // it will be used by several methods
+    private static Scanner scanner = new Scanner(System.in);
 
-  public static double readNumber(String prompt, double min, double max) {
-      double value;
-      while (true) {
-          System.out.print(prompt);
-          value = scanner.nextDouble();
-          if (value >= min && value <= max)
-              break;
-          System.out.println("Enter a value between " + min + " and " + max);
-      }
-      return value;
-  }
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
+    // // method overloading
+    public static double readNumber(String prompt, double min, double max) {
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
 }
