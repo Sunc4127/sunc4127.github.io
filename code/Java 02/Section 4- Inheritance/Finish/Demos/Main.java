86866a3949846c084f5b4af19dac67f91f00dbd1
Sunc4127 <sccplay888@gmail.com>
2021-12-25 16:57:18 +0800

    add notes

--- code/Java 02/Section 4- Inheritance/Finish/Demos/Main.java
+++ code/Java 02/Section 4- Inheritance/Finish/Demos/Main.java
@@ -1,7 +1,6 @@
 package com.codewithmosh;
 
 public class Main {
-
     public static void main(String[] args) {
        UIControl[] controls = { new TextBox(), new CheckBox() };
        for (var control : controls)
