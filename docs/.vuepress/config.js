60d5a8f05a468d23512d8477a56acc73433bacff
Sunc4127 <sccplay888@gmail.com>
2022-12-04 16:55:22 +0800

    add more files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,10 @@ const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
-    //theme: "vdoing", // 使用npm包主题
-    theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+    theme: "vdoing", // 使用npm包主题
+    //theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-    title: "Scott's Blog",
+    title: "Scott の 博客",
     description:
         "web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。",
     // base: '/', // 格式：'/<仓库名>/'， 默认'/'
