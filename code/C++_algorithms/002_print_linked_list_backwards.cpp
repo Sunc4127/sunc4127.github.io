ac6822b77cfa555d9c40432f6832a379769037b6
Sunc4127 <sccplay888@gmail.com>
2023-06-24 20:27:52 +0800

    Update 002_print_linked_list_backwards.cpp

---
+++ code/C++_algorithms/002_print_linked_list_backwards.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    vector<int> reversePrint(ListNode *head)
+    {
+        stack<int> stk;
+        while (head != nullptr)
+        {
+            stk.push(head->val);
+            head = head->next;
+        }
+        vector<int> res;
+        while (!stk.empty())
+        {
+            res.push_back(stk.top());
+            stk.pop();
+        }
+        return res;
+    }
+};
