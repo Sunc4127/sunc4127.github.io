b8aca221073ad5837829af26979d34056dce945b
Cheng Sun (Scott) <sccplay888@gmail.com>
2023-06-26 21:52:12 +0800

    Update 005_reverse_list.cpp

--- code/C++_algorithms/005_reverse_list.cpp
+++ code/C++_algorithms/005_reverse_list.cpp
@@ -24,3 +24,22 @@ public:
         return left;
     }
 };
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        return recur(nullptr, head); // 调用递归并返回
+    }
+
+private:
+    ListNode *recur(ListNode *left, ListNode *right)
+    {
+        if (right == nullptr)
+            return left;                           // 终止条件
+        ListNode *res = recur(right, right->next); // 递归后继节点
+        right->next = left;                        // 修改节点引用指向
+        return res;                                // 返回反转链表的头节点
+    }
+};
